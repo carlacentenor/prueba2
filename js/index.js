ae96a03728719d6884c5db524cd947b5c1070457
carlacentenor <carlacentenor@gmail.com>
2018-01-12 18:39:29 -0500

    jjsjasas

--- js/index.js
+++ js/index.js
@@ -1,17 +1,6 @@
 $(document).ready(function()
  {
-    // Inicializar la base de datos
-    var config = {
-      apiKey: "AIzaSyBrzy_DeH15B9jdEQBD40kFAo1Egw1tVNk",
-      authDomain: "veigapp-e9b22.firebaseapp.com",
-      databaseURL: "https://veigapp-e9b22.firebaseio.com",
-      projectId: "veigapp-e9b22",
-      storageBucket: "veigapp-e9b22.appspot.com",
-      messagingSenderId: "465534514371"
-
-    };
-
-    firebase.initializeApp(config);
+  
 
     var database = firebase.database();
 
