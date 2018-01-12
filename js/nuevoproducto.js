c39215cf7917776d56b886644244fd3c71a5cb74
carlacentenor <carlacentenor@gmail.com>
2018-01-12 18:42:12 -0500

    ddsdsd

--- js/nuevoproducto.js
+++ js/nuevoproducto.js
@@ -14,101 +14,7 @@ $(document).ready(function()
       firebase.initializeApp(config);
 
 
-    var database = firebase.database();
 
-    var articulo;
-    var descripcion;
-    var precio;
-    
-
-    $("#imagen").change(function()
-    {
-        var descriptor=new FileReader();
-        descriptor.readAsDataURL(this.files[0]);
-
-        descriptor.onloadend = function()
-        {
-            imagen=descriptor.result;
-            $("#previsualizacion").attr("src",imagen);
-        };
-    });
-
-
-    $("#formularioAlta").change(function()
-    {
-        articulo=$("#articulo").val();
-        descripcion=$("#descripcion").val();
-        precio=$("#precio").val();
-
-        if (articulo && descripcion && precio)
-        {
-            $("#botonGuardar").prop("disabled",false);
-        }
-        else
-        {
-            $("#botonGuardar").prop("disabled",true);
-        }
-
-    });
-
-
-    $("#botonGuardar").click(function()
-    {
-        articulo=$("#articulo").val();
-        descripcion=$("#descripcion").val();
-        precio=$("#precio").val();
-
-        if (!imagen)
-        {
-            imagen="NONE";
-        }
-
-        // Indicamos que la referencia base de nuestra base de datos es productos (algo así como el padre)
-        // del que colgarán el resto de nodos hijos.
-        /*
-        var usersRef = new Firebase('https://samplechat.firebaseio-demo.com/users');
-        var fredRef = usersRef.child('fred');
-        var fredFirstNameRef = fredRef.child('name/first');
-        */
-        var referencia=database.ref("productos");
-
-
-        // De la siguiente forma el método sobreescribe los datos
-    /*
-        referencia.set(
-        {
-            articulo: articulo,
-            descripcion: descripcion,
-            precio: precio,
-            imagen: imagen
-        });
-        */
-
-        // Ahora estamos poniendo el articulo como clave en la colección
-        // De esta manera podremos añadir nuevos articulos o actualizar uno ya existente.
-
-    /*
-        referencia.child(articulo).set(
-        {
-            descripcion: descripcion,
-            precio: precio,
-            imagen: imagen
-        });
-        */
-
-        // Si queremos permitir que hayas artículos con nombres duplicados entonces tendremos
-        // que decirle a Firebase que utilice otra clave en lugar del nombre del articulo.
-        // Usaremos el método push en lugar de set
-        referencia.push(
-        {
-            articulo: articulo,
-            descripcion: descripcion,
-            precio: precio,
-            imagen: imagen
-        },function()
-        {
-            alert('El alta se ha realizado correctamente');
-        });
 
         $('.btn-google').click(function(){
             event.preventDefault();
@@ -122,7 +28,7 @@ $(document).ready(function()
              var user = result.user;
             });
         })
-    });
+   
 
 
 
